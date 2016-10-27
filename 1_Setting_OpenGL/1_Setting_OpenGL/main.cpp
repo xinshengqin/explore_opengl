cd4413a873cee6526e8413bb5d70d584cbe4e601
xinshengqin <xinshengqin2014@gmail.com>
2016-10-27 01:42:07 -0700

    use vao and vbo to draw a triangle

--- 1_Setting_OpenGL/1_Setting_OpenGL/main.cpp
+++ 1_Setting_OpenGL/1_Setting_OpenGL/main.cpp
@@ -1,5 +1,6 @@
 #include "Dependencies\glew\glew.h"
 #include "Dependencies\freeglut\freeglut.h"
+#include "Core\GameModels.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -8,21 +9,28 @@
 
 using namespace Core;
 
+Models::GameModels* gameModels;
 GLuint program;
+GLuint vertex_array_object;
+
 
 void renderScene(void)
 {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0, 0.0, 0.0, 1.0);//clear red
+	glClearColor(0.0, 0.3, 0.3, 1.0);
 
+	glBindVertexArray(gameModels->GetModel("triangle1"));
 	//use the created program
 	glUseProgram(program);
-
-	//draw 3 vertices as triangles
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-
 	glutSwapBuffers();
+}
+void closeCallback()
+{
+
+	std::cout << "GLUT:\t Finished" << std::endl;
+	glutLeaveMainLoop();
 }
 
 void Init()
@@ -30,10 +38,13 @@ void Init()
 
 	glEnable(GL_DEPTH_TEST);
 
+	gameModels = new Models::GameModels();
+	gameModels->CreateTriangleModel("triangle1");
 	//load and compile shaders
 	Core::Shader_Loader shaderLoader;
 	program = shaderLoader.CreateProgram("Shaders\\Vertex_Shader.glsl",
 		"Shaders\\Fragment_Shader.glsl");
+	//generate the vertex array
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
@@ -46,6 +57,7 @@ int main(int argc, char **argv)
 	glutInitWindowPosition(500, 500);//optional
 	glutInitWindowSize(800, 600); //optional
 	glutCreateWindow("Drawing my first triangle");
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
 
 	glewInit();
 	if (glewIsSupported("GL_VERSION_4_4"))
@@ -61,7 +73,9 @@ int main(int argc, char **argv)
 
 	// register callbacks
 	glutDisplayFunc(renderScene);
+	glutCloseFunc(closeCallback);
 	glutMainLoop();
+	delete gameModels;
 	glDeleteProgram(program);
 	return 0;
 }
