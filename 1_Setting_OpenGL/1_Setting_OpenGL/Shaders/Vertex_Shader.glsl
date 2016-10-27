66f1da76c715bd1df82434480af4c2d04714ca9c
xinshengqin <xinshengqin2014@gmail.com>
2016-10-27 01:49:49 -0700

    adjust location of shaders folder

--- 1_Setting_OpenGL/1_Setting_OpenGL/Shaders/Vertex_Shader.glsl
+++
@@ -1,7 +0,0 @@
-#version 440 core                           //lower your version if GLSL 4.5 is not supported by your GPU
-layout(location = 0) in vec3 in_position;  //set the frist input on location (index) 0 ; in_position is our attribute 
- 
-void main()
-{
-  gl_Position = vec4(in_position, 1.0);//w is 1.0, also notice cast to a vec4
-}
